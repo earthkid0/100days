6f9b826753dae81b08efd522e15216b1f3489311
earthkid0 <ghs1541@naver.com>
2022-01-07 19:40:30 +0900

    2days tip calculator

--- 2days/tip calculator.py
+++ 2days/tip calculator.py
@@ -1,13 +1,10 @@
 #내야할 금액
 pay = float(input("내야 할 금액 = "))
-#내고싶은 팁 10,12,15
+print("내야할 금액 = ${0}")
 tip = int(input("내고 싶은 팁 10,12,15 ? = "))
-tip = tip * (1/100)
 sum = round((pay * tip) + pay , 2)
-print("tip = {0}".format(sum))
-#같이온 인원수 
+print("tip = ${0}".format(sum))
 people = int(input("같이 온 인원 = "))
 print("{0}명".format(people))
-#내야할 금액
 finally_pay = round(sum / (people), 2)
-print("총 내야할 금액 {0}".format(finally_pay))
+print("총 내야할 금액 ${0}".format(finally_pay))
