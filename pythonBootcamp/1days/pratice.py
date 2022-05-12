e777c1a91b5ed361da63d8e66a0ab8c3f7161e66
earthkid0 <ghs1541@naver.com>
2022-05-12 20:11:38 +0900

    파일명 수정

---
+++ pythonBootcamp/1days/pratice.py
@@ -0,0 +1,13 @@
+from random import *
+
+# a = "01041476128"
+# print(f"제 전화번호 뒷자리는{a[7:]}")
+
+
+for i in range(0, 10):
+    i += 1
+    print(i)
+    if i == 5:
+        break
+
+print(randint(0, 5))
