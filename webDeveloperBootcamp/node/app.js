cee973fbb3e42f00188de9c6c48bda24b013c16e
earthkid0 <ghs1541@naver.com>
2022-06-04 19:01:52 +0900

    node.js

---
+++ webDeveloperBootcamp/node/app.js
@@ -0,0 +1,17 @@
+// setTimeout(() => {
+//     for (let i = 0; i < 10; i++) {
+//         console.log(i)
+//     }
+// }, 3000);
+
+// let nums = [1, 2, 3, 4];
+// nums.forEach(e => {
+//     console.log(e + 1)
+// });
+
+const { add, square } = require("./math")
+
+console.log(add(2, 3));
+console.log(square(3))
+
+
---
+++ webDeveloperBootcamp/node/math.js
@@ -0,0 +1,10 @@
+// const add = (x, y) => x + y;
+// const square = (x) => x * x;
+
+// exports.add = add; // module.exports
+// exports.square = square;
+
+module.exports = {
+    add: (x, y) => x + y,
+    square: (x) => x * x,
+}
