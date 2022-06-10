277360506d7527aacf4f160d344727695056bd16
earthkid0 <ghs1541@naver.com>
2022-06-10 10:50:47 +0900

    RESTful GET

--- webDeveloperBootcamp/node/restful/index.js
+++ webDeveloperBootcamp/node/restful/index.js
@@ -29,20 +29,20 @@ let comments = [
 ]
 
 app.get('/comments', (req, res) => {
-    res.render('comments/index', { comments })
+    res.render('comments/index', { comments });
 })
 
 app.get('/tacos', (req, res) => {
-    res.send('get/tacos!')
-    console.log(req.params)
+    res.send('get/tacos!');
+    console.log(req.params);
 })
 
 app.post('/tacos', (req, res) => {
-    const { meat, qty } = req.body
-    res.send(`meat:${meat} qty:${qty}`)
-    console.log(req.body)
+    const { meat, qty } = req.body;
+    res.send(`meat:${meat} qty:${qty}`);
+    console.log(req.body);
 })
 
 app.listen(8000, () => {
-    console.log("port: 8000")
+    console.log("port: 8000");
 })
